1008a02dd177366090c74bd0e26c25e3e072f940
Oh TaeJun <rewrite0w0@gmail.com>
2021-03-05 18:26:22 +0900

    delete buttons

--- main.js
+++ main.js
@@ -1,53 +1,45 @@
 const mainInput = document.querySelector(".mainInput");
-const mainButton = document.querySelector(".mainButton");
 const mainTodo = document.querySelector(".mainTodo");
 
-const addTodo = (button, input) => {
-  button.addEventListener("click", (click) => {
-    click.preventDefault();
-
-    if (input.textLength !== 0) {
+const addTodo = (input) => {
+  input.addEventListener("keyup", (inputkey) => {
+    if (input.textLength !== 0 && inputkey.keyCode === 13) {
+      inputkey.preventDefault();
       const makeContainer = document.createElement("div");
       makeContainer.classList.add("todoContainer");
       mainTodo.appendChild(makeContainer);
 
-      const MakeLi = document.createElement("li");
-      MakeLi.classList.add("todo");
-      MakeLi.textContent = input.value;
-      makeContainer.appendChild(MakeLi);
-
-      const makeCompleteButton = document.createElement("button");
-      makeCompleteButton.classList.add();
-      MakeLi.appendChild(makeCompleteButton).textContent = `완료`;
-
-      const makeReplaceButton = document.createElement("button");
-      makeReplaceButton.classList.add();
-      MakeLi.appendChild(makeReplaceButton).textContent = `수정`;
+      const makeLi = document.createElement("li");
+      makeLi.classList.add("todo-item");
+      makeLi.textContent = input.value;
+      makeContainer.appendChild(makeLi);
+      input.value = "";
 
       const makeDeleteButton = document.createElement("button");
-      makeDeleteButton.classList.add();
-      MakeLi.appendChild(makeDeleteButton).textContent = `삭제`;
+      makeDeleteButton.classList.add("todoDelete");
+      makeLi.appendChild(makeDeleteButton).textContent = `❌`;
     } else {
       return 0;
     }
   });
 };
 
-// const removeTodo = (button) => {
-//   button.addEventListener("click", (removeTodo) => {
-//     removeTodo.remove();
-//   });
-// };
-
-const changeTodo = () => {};
-
-const completeTodo = () => {};
+const removeTodo = () => {
+  addEventListener("click", (e) => {
+    const finder = e.target;
+    const removeTarget = finder.parentElement;
+    if (finder.classList[0] === "todoDelete") {
+      // console.log(removeTarget);
+      removeTarget.remove();
+    } else {
+      return 0;
+    }
+  });
+};
 
 const init = () => {
-  addTodo(mainButton, mainInput);
-  // removeTodo();
-  // changeTodo();
-  // completeTodo();
+  addTodo(mainInput);
+  removeTodo();
 };
 
 init();
