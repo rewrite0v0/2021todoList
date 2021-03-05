c2d1505593a4922e8ab977cdb25fb2a1b4096a27
Oh TaeJun <rewrite0w0@gmail.com>
2021-03-05 23:00:28 +0900

    done..? toggle line

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const addTodo = (input) => {
     if (input.textLength !== 0 && inputkey.keyCode === 13) {
       inputkey.preventDefault();
       const makeContainer = document.createElement("div");
-      makeContainer.classList.add("todoContainer");
+      makeContainer.classList.add("todoWrap");
       mainTodo.appendChild(makeContainer);
 
       const makeLi = document.createElement("li");
@@ -37,9 +37,32 @@ const removeTodo = () => {
   });
 };
 
+const completeTodo = () => {
+  addEventListener("click", (e) => {
+    const finder = e.target;
+    const picker_on = finder.classList[0];
+    const picker_off = finder.classList[1];
+
+    if (picker_on === "todo-item") {
+      finder.style.textDecorationLine = "line-through";
+      finder.classList.add("todoDone");
+    }
+    if (picker_off === "todoDone") {
+      finder.style.textDecorationLine = "none";
+      finder.classList.remove("todoDone");
+    }
+  });
+};
+
+const focus = (here) => {
+  here.focus();
+};
+
 const init = () => {
+  focus(mainInput);
   addTodo(mainInput);
   removeTodo();
+  completeTodo();
 };
 
 init();
